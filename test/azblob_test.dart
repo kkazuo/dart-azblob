3b5a7646c49460ebaebdb474bf65d8fd07415bc8
Koga Kazuo <obiwanko@me.com>
2020-02-09 19:27:03 +0900

    Fmt

--- test/azblob_test.dart
+++ test/azblob_test.dart
@@ -3,8 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    setUp(() {
-    });
+    setUp(() {});
 
     test('First Test', () {
       expect(true, isTrue);
